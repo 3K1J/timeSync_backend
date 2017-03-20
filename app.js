963b15ec86310f039c22e6a496a8971f2a524cf5
Ryan Phillips <rphillips4417@gmail.com>
2017-03-20 16:57:00 -0600

    Passport mostly complete

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 
-
+const logger = require('morgan')
 const cors = require('cors')
-
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080
- 
+const session = require('express-session')
+
+// const passportLocal = require('passport-local')
+
+const passport = require('./passport')
+
+
+app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(session({
+  secret: 'ch1cken nugget',
+  saveUninitialized: true,
+  resave: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(express.static('Public'))
 app.use(cors())
+
 
 
 app.listen(PORT, ()=> {
