3f89197d10a046cf8df36bfda993859e56735e83
NickSugar <nicksugar1@outlook.com>
2017-03-21 13:38:51 -0600

    saved seeds

--- seeds/dates.js
+++ seeds/dates.js
@@ -5,13 +5,24 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('dates').insert([
-        {start: 'March 21st 2017, 07:30:00 am', end: 'March 21st 2017, 08:00:00 am',
+        {date: 'March 21st 2017',
+        start: '07:30:00 am',
+        end: '08:00:00 am',
         event_id: knex('events').where('title', 'Breakfast').select('id')},
-        {start: 'March 21st 2017, 11:30:00 am', end: 'March 21st 2017, 12:00:00 pm',
+
+        {date: 'May 11th 2017',
+        start: '11:30:00 am',
+        end: '12:00:00 pm',
         event_id: knex('events').where('title', 'Lunch').select('id')},
-        {start: 'March 21st 2017, 05:30:00 pm', end: 'March 21st 2017, 06:00:00 pm',
+
+        {date: 'October 2nd 2018',
+        start: '05:30:00 pm',
+        end: '06:00:00 pm',
         event_id: knex('events').where('title', 'Dinner').select('id')},
-        {start: 'March 21st 2017, 07:30:00 pm', end: 'March 21st 2017, 08:00:00 pm',
+
+        {date: 'July 4th 2019',
+        start: '07:30:00 pm',
+        end: '08:00:00 pm',
         event_id: knex('events').where('title', 'Dessert').select('id')}
       ]);
     });
