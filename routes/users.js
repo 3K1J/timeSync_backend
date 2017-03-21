61336f36369984b0ad45fb73e2d01bc61fcf0c3d
Ryan Phillips <rphillips4417@gmail.com>
2017-03-21 15:16:07 -0600

    User routes set up

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+
+function User() {return knex('users')}
+
+// **************************** CREATE ***********************************
+
+// ^^^^^^ Create User ^^^^^^
+
+router.post('/', function(req, res){
+
+  Users().insert({
+    name: req.body.name
+    email: req.body.email
+  }, ['id', 'name', 'email']).then(function(result){
+    res.json(result);
+  })
+})
+
+// **************************** READ ***********************************
+
+// ^^^^^^ Read User ^^^^^^
+
+router.get('/', function (req, res){
+
+  knex('users').select().then(function(result){
+    res.json(result)
+  })
+})
+
+// ^^^^^^ Read One User ^^^^^^
+
+router.get('/', function(req, res){
+  knex('users').where('id', req.params.id).first().then(function(result){
+    res.json(result)
+  })
+})
