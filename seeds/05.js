a17f0ab95e6b444df155fa42d998f9f9c59db50e
NickSugar <nicksugar1@outlook.com>
2017-03-22 10:03:31 -0600

    fixed seeds and query

--- seeds/05.js
+++ seeds/05.js
@@ -4,16 +4,16 @@ exports.seed = function(knex) {
   return knex('dates_users').del()
     .then(function () {
       return knex('dates_users').insert([
-        {date_id: knex('dates').where('start', 'March 21st 2017, 07:30:00 am').select('id'),
+        {date_id: knex('dates').where('date', 'March 21st 2017, 07:30:00 am').select('id'),
         user_id: knex('users').where('name', 'Paul Torres').select('id')},
 
-        {date_id: knex('dates').where('start', 'March 21st 2017, 11:30:00 am').select('id'),
+        {date_id: knex('dates').where('date', 'March 21st 2017, 11:30:00 am').select('id'),
         user_id: knex('users').where('name', 'Ryan Phillips').select('id')},
 
-        {date_id: knex('dates').where('start', 'March 21st 2017, 05:30:00 pm').select('id'),
+        {date_id: knex('dates').where('date', 'March 21st 2017, 05:30:00 pm').select('id'),
         user_id: knex('users').where('name', 'Bill Boughton').select('id')},
 
-        {date_id: knex('dates').where('start', 'March 21st 2017, 07:30:00 pm').select('id'),
+        {date_id: knex('dates').where('date', 'March 21st 2017, 07:30:00 pm').select('id'),
         user_id: knex('users').where('name', 'Brad Folkers').select('id')}
       ]);
     });
